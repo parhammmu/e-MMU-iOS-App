018bccfb96ecc48d48f231a7676fd7b7ed08b5ac
Parham Majdabadi <parhammajdbadiuk@gmail.com>
2014-12-23 14:11:23 +0000

    Adding Parse and Facebook frameworks

--- e-MMU/AppDelegate.swift
+++ e-MMU/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Parse setup
+        ParseCrashReporting.enable()
+        Parse.setApplicationId("oBYGdBE3UxlDD5eoGOoLT8SzXsk9wa3sinAtyCPK", clientKey: "pK14x4Xl4jSsRrHsYWKldF7BlS1inVo02yyv1yiW")
+        //Parse.enableLocalDatastore()
+    
         return true
     }
 
@@ -35,10 +40,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBAppCall.handleDidBecomeActiveWithSession(PFFacebookUtils.session())
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        PFFacebookUtils.session().close()
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        return FBAppCall.handleOpenURL(url, sourceApplication: sourceApplication, withSession: PFFacebookUtils.session())
     }
 
 
